d5a00b00cd6d8754717aac689e3ec912c8fd0be0
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 04:30:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,15 @@
 #write your code here
 
-def countdown
-  number = 10
+def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
-    number -= 1
   end
   return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0
+    number -= 1 
+    sleep(1)
+  end
 end
